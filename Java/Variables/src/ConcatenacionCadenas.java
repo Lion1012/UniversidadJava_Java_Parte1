bf5317aa3466506e06ab3f89faec28880709f4f8
Lion1012 <liondev1012@gmail.com>
2024-10-07 01:58:14 +0200

    Finalizo ejemplo de ConcatenacionCadenas.java

---
+++ Java/Variables/src/ConcatenacionCadenas.java
@@ -0,0 +1,8 @@
+public class ConcatenacionCadenas {
+    public static void main(String[] args) {
+        System.out.println("*** Concatenacion de Cadenas ***"); // Vamos a demostrar el uso de concatenaciones en cadenas, creando una cadena nueva con la union de las dos creadas, utilizando el operador +
+        var nombre= "Angel";
+        var apellido = "Leon";
+        var nombreCompleto = nombre + " " + apellido;
+    }
+}
